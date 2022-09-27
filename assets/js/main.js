e662a06b19b8a3858d95dd0616392f5d560a06e2
Dominique <domjon24@gmail.com>
2022-09-27 18:11:16 -0500

    removed test code. added html, css & js. stopped at 41:06

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,19 @@
 // Grab elements
+const selectElement = selector => {
+    const element = document.querySelector(selector)
+    if(element) return element;
+    throw new Error(`Something went wrong :( make sure ${selector} exists or is typed correctly`)
+};
+const scrollHeader = () => {
+    const headerElement = selectElement('#header');
+    if(this.scrollY >= 15) {
+        headerElement.classList.add('actvated')
+    }else {
+        headerElement.classList.remove('actvated')
+    }
+}
+window.addEventListener('scroll', scrollHeader);
 
-//Nav styles on scroll
 
 // Open menu & search pop-up
 
